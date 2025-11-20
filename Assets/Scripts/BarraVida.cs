5e1a73c119b9c438e6bed59665a38edf4749a3d3
ICarlos-18 <csanchezhernandez567@gmail.com>
2025-11-20 00:17:54 -0600

    Merging Correct Changes

---
+++ Assets/Scripts/BarraVida.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraVida : MonoBehaviour
+{
+    public Image rellenoBarraVida;
+    private Player player;
+    private float vidaMaxima;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        vidaMaxima = player.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rellenoBarraVida.fillAmount = player.health / vidaMaxima;
+    }
+}
